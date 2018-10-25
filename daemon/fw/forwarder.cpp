aa2a1b230b4b41fb1f68b4ae94a1297bdd76f212
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-10-25 00:41:40 -0400

    wip

--- daemon/fw/forwarder.cpp
+++ daemon/fw/forwarder.cpp
@@ -344,7 +344,7 @@ Forwarder::onIncomingData(Face& inFace, const Data& data)
 	
   	//newDataTag = interestInPit.getTag<lp::newDataTag>();
 	// This happens at Producer
-	NFD_LOG_INFO("onincomingdata fresh data: " << data.getName() << "  " << *newDataTag << "  " <<  *newDataTag + 1 );
+	NFD_LOG_INFO("onincomingdata fresh data: " << data.getName() << "  " << *newDataTag  );
 	
 	/*
 	if (*newDataTag  == 0) {
