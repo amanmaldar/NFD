46a9744396f786063caa519b7b54e9ba77e5e294
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-10-28 21:25:27 -0400

    wip

--- core/global.cpp
+++ core/global.cpp
@@ -11,18 +11,18 @@ namespace cs {
 			ofs.open (path, std::fstream::in | std::fstream::out | std::fstream::app);
 				ofs	<< "CS_Metrics\n"
 					<< "    TotalRespTime="<< nm.responseTime << "\n"
-					<< "    AvgRespTime="<< nm.responseTime/ndnPerf::nInData << "\n"
+					<< "    AvgRespTime="<< nm.responseTime/nm.nInData << "\n"
 					<< "    TotalFwdLatency=" << nm.fwdLatencyTag <<"\n"
-					<< "    AvgFwdLatency=" << nm.fwdLatencyTag/ndnPerf::nInData <<"\n"
+					<< "    AvgFwdLatency=" << nm.fwdLatencyTag/nm.nInData <<"\n"
 					<< "    TotalInDataPackets=" << nm.nInData << "\n";
 			ofs.close();
 		
 		}
 		
 		void perfMeasure::clearNwMetrics(networkMetrics &nm){
-			nm->responseTime = 0;
-			nm->fwdLatencyTag = 0; 
-			nm->nInData = 0;
+			nm.responseTime = 0;
+			*nm->fwdLatencyTag = 0; 
+			*nm.nInData = 0;
 		}
 
 
