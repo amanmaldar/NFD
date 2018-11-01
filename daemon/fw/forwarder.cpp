2283660cda2f8d0a1e84211927620aebc4769338
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-11-01 18:49:44 -0400

    total processing late on path

--- daemon/fw/forwarder.cpp
+++ daemon/fw/forwarder.cpp
@@ -439,7 +439,7 @@ Forwarder::onIncomingData(Face& inFace, const Data& data)
 			nm.nInData++;
 			nm.fwdLatencyTag += fwdLatency;
 			nm.responseTime += responseTime;
-			nm.processLat + = *processLat;
+			nm.processLat += *processLat;
 			NFD_LOG_DEBUG("onincomingdata non_local" << data.getName());
 		}
 	}
