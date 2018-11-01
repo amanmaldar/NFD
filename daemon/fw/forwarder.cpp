8ce7074d9c580407d32373928329fdaaf2d4cf67
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-11-01 13:21:53 -0400

    fixes

--- daemon/fw/forwarder.cpp
+++ daemon/fw/forwarder.cpp
@@ -278,6 +278,7 @@ Forwarder::onContentStoreHit(const Face& inFace, const shared_ptr<pit::Entry>& p
 			nm.responseTime += responseTime;
 			NFD_LOG_DEBUG("cshits non_local" << data.getName());
 		}
+	}
 
 
   pitEntry->isSatisfied = true;
